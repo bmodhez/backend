61efb540e0e6fc82b9e55f7458d3a15460cd079f
bmodhez <modhbhavin05@gmail.com>
2025-12-26 09:55:01 +0530

    Fixed the dashboard

--- AssetManagement/assets/views/dash_views.py
+++ AssetManagement/assets/views/dash_views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from assets.models import Asset, License, Accessory, Consumable, Component
+from assets.models.asset import Asset
+from assets.models.license import License
+from assets.models.accessories import Accessory
+from assets.models.consumables import Consumable
+from assets.models.components import Component
 
-@login_required
+
 def dashboard(request):
-    return render(request, "dashboard/dashboard.html", {
-    "assets": Asset.objects.all(),
-    "licenses": License.objects.all(),
-    "accessories": Accessory.objects.all(),
-    "consumables": Consumable.objects.all(),
-    "components": Component.objects.all(),
-})
+    assets_count = Asset.objects.count()
+    licenses_count = License.objects.count()
+    accessories_count = Accessory.objects.count()
+    consumables_count = Consumable.objects.count()
+    components_count = Component.objects.count()
+
+    context = {
+        "assets_count": assets_count,
+        "licenses_count": licenses_count,
+        "accessories_count": accessories_count,
+        "consumables_count": consumables_count,
+        "components_count": components_count,
+    }
+
+    return render(request, "dashboard/dashboard.html", context)
